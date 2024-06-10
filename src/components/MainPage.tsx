e00cd1437c059f4cdc561ef2b2312c8fd5ebf28a
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2024-06-10 18:17:04 +0530

    Data reading and writing done Error at MainPage.tsx line 30, isConnected is false and was expected to be true

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -25,7 +25,26 @@ const MainPage = () => {
       portRef.current = port;
       const reader = port.readable?.getReader();
       readerRef.current = reader;
-      // Handle data reading from the port
+      const decoder = new TextDecoder();
+      let lineBuffer = "";
+      console.log(isConnected); //Error : this is false somehow
+      while (!isConnected) {
+        const StreamData = await reader?.read();
+        if (StreamData?.done) {
+          console.log("Stream done");
+          break;
+        }
+        const receivedData = decoder.decode(StreamData?.value, {
+          stream: true,
+        });
+        const lines = (lineBuffer + receivedData).split("\n");
+        lineBuffer = lines.pop() ?? "";
+        for (const line of lines) {
+          const dataValues = line.split(",");
+          //   processData(dataValues);
+          console.log(`Received Data: ${line}`);
+        }
+      }
     } catch (error) {
       console.error("Error connecting to device:", error);
       disconnectDevice();
@@ -47,11 +66,31 @@ const MainPage = () => {
     }
   };
 
+  const writeData = async (data: string) => {
+    try {
+      if (isConnected && portRef.current && portRef.current.writable) {
+        const writer = portRef.current.writable.getWriter();
+        const encoder = new TextEncoder();
+        const dataToSend = encoder.encode(`${data}\n`);
+        await writer.write(dataToSend);
+        writer.releaseLock();
+        console.log("Data sent");
+      } else {
+        alert("Device not connected");
+      }
+    } catch (error) {
+      console.error("Error writing data to device:", error);
+    }
+  };
+
   return (
     <div className="flex h-[80%] justify-center items-center">
-      <div className="">
+      <div className="flex gap-4">
         <Button className="bg-primary" onClick={handleClick}>
           {isConnected ? "Disconnect" : "Connect"}
+        </Button>
+        <Button className="bg-primary" onClick={() => writeData("n")}>
+          Write
         </Button>
       </div>
     </div>
